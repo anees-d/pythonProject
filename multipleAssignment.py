0bc52a74f0fba31598f768f6cb4db2386ffe14cd
anees-d <qualitytech585@gmail.com>
2025-01-24 02:01:25 +0500

    python code practice

---
+++ multipleAssignment.py
@@ -0,0 +1,11 @@
+#Multiple assignment = allow us to assign multiple variables at the same time in one line of code.
+
+#name = "Bro"
+#age = 21
+#attractive = True
+
+#name, age, attractive = "Bro", 21, True
+
+print(name)
+print(age)
+print(attractive)
