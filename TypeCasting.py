d8b71a69bb212057a4b32b73edfbc740609fb50b
anees-d <qualitytech585@gmail.com>
2025-01-28 02:25:38 +0500

    python code practice new

---
+++ TypeCasting.py
@@ -0,0 +1,19 @@
+#Data type = converts a data type to another data type
+
+x  =  1  #int
+y = 2.0 #float
+z = "3" #str
+
+x = float(x)
+y = int(y)
+z = int(z)
+
+
+print(x)
+print(int(y))
+print(z*3)
+
+#str
+print("x is  "+str(x))
+print("y is "+str(y))
+print(z*3)
---
+++ userinput.py
@@ -0,0 +1,10 @@
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))
+height = float(input("How tall ar you?: "))
+
+age = age+1
+
+print("Hello " +name)
+print("you are "+str(age)+" year old")
+print("You are "+str(height)+" cm tall")
+
